3e9614367b639d6f125c91480a9be88f11fe96aa
Ibrahim Kurhan <Ibrahim.Kurhan@Nology.local>
2020-04-03 13:55:02 +0100

    wasp game

--- app.js
+++ app.js
@@ -1,89 +1,89 @@
-class Wasp {
-    constructor(sp, name, hitPoint) {
-        this.sp = sp;
+// # Wasp Game 
+// You may only use JS / CSS / HTML for this task and no frameworks or libraries. 
+// You can use sessions. The whole program should be written in one single file and may be either procedural or 
+// object oriented. It should be possible to place the file anywhere within a website and for it to be run in a 
+// browser. 
+// Create a simple text only game with a single button that hits a random wasp from a nest of wasps. 
+// Each time the button is pressed, a wasp is hit and its points reduced. Once 
+// its points have gone to zero the wasp dies and cannot be hit again. 
+// The screen shows the state of all the wasps after each hit. 
+// The game is over when all wasps have died. You should be able to start a new game 
+// once the game is over. 
+// You should be able to solve this in under 150 lines including comments and any more than that 
+// is a sign you have over complicated the problem. You can use basic html and css to tidy up the output, 
+// but the solution should be simple text and a button rather than any styled UI. 
+// You start the game with the following wasps. 
+// ## 1 x Queen  
+// * 80 Hit Points  
+// * Loses 7 hit points every time it is hit  
+// * All wasps die if the queen is killed 
+// ## 5 x Worker wasps  
+// * Each one starts with 68 hit points  
+// * Each one Loses 10 hit points each time it is hit 
+// ## 8 x Drone wasps  
+// * Each starts with 60 hit points  
+// * Each loses 12 hit points each time it is hit. 
+// This task should take no more than 60 - 90 minutes to complete. Send us the final HTML file.
+//  Be prepared to talk through your solution. 
+
+class Wasps {
+    constructor(name, lives) {
         this.name = name;
-        this.hitPoint = hitPoint;
-    }
-
-    losePoints() {
-        const afterHit = this.sp - this.hitPoint;
-        if(afterHit > 0){
-            this.sp -= this.hitPoint;
-        } else {
-            this.sp = 'dead';
-            if(this.name === 'Queen') {
-                alert('game over')
-            }
-        }
-    }
-
-    get html() {
-        return `<div class="wrap">${this.name}<div id="first">${this.sp}</div></div>`;
+        this.lives = lives;
     }
 }
-let wasp = [];
-// ===============================================================================================
-class Queen extends Wasp {
+
+class Queen extends Wasps {
     constructor() {
-        super(80, "Queen", 7);
+        super("Queen", 80);
+    }
+    attack() {
+        this.lives -= 7;
     }
 }
 
-class Worker extends Wasp {
+class Worker extends Wasps {
     constructor() {
-        super(68, "Worker", 12);
+        super("Worker", 68);
+    }
+    attack() {
+        this.lives -= 10;
     }
 }
 
-class Drone extends Wasp {
+class Drone extends Wasps {
     constructor() {
-        super(60, "Drone", 10);
+        super("Drone", 60);
+    }
+    attack() {
+        this.lives -= 12;
     }
 }
 
-// newDrone.reducedPoint();
-// console.log(newDrone.hitPoints);
-
-document.querySelector(".A").addEventListener("click", () => {
-    let randomNumber = Math.floor(Math.random() * wasp.length);
-    const randomWasp = wasp[randomNumber];
-    randomWasp.losePoints();
-    // console.log(randomNumber);
-    update();
-});
-
-const wrap = document.querySelector(".wrap");
-
-const update = () => {
-    wrap.innerHTML = "";
-    wasp.forEach(insect => {
-        wrap.innerHTML += insect.html;
-    });
-}
-
-const start = () => {
-
-    // let boxes = [{id:1,name:'Queen',life:80},{id:2,name:'Worker',life:10},{name:'Drone',life:20},{name:'Drone',life:20}];
-
-
-    wrap.innerHTML = "";
-    const newQueen = new Queen();
-    wasp.push(newQueen);
-    wrap.innerHTML += newQueen.html;
-
+const reset = () => {
+    wasps = [];
+    wasps.push(new Queen())
+    for (let i = 0; i < 8; i++) {
+        wasps.push(new Drone())
+    }
     for (let i = 0; i < 5; i++) {
-        const newWorker = new Worker();
-        wasp.push(newWorker);
-        wrap.innerHTML += newWorker.html;
+        wasps.push(new Worker())
     }
 
-    for (let i = 0; i < 9; i++) {
-        const newDrone = new Drone();
-        wasp.push(newDrone);
-        wrap.innerHTML += newDrone.html;
-    }
 }
 
-start();
+let wasps = [];
+reset();
+const displayWaspsInThePage = () => {
+    const alivewasps = wasps.filter(wasp => (wasp.lives > 0))
+    const content = alivewasps.map(wasp => `<div class="wasp">${wasp.name}<span class="health">${wasp.lives}</span></div>`)
+    document.querySelector(".wasps").innerHTML = content.join("");
+}
+displayWaspsInThePage();
 
-
+document.querySelector(".kill").addEventListener("click", function () {
+    const randomNumber = Math.floor(Math.random() * wasps.length);
+    const randomWasp = wasps[randomNumber];
+    randomWasp.attack();
+    displayWaspsInThePage();
+});
